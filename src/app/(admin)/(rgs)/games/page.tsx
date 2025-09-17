afc8d99279cbdf138eb6242b64fb19bf47d443a7
Andrii Vdovychenko <fisher377164@gmail.com>
2025-09-17 20:59:50 +0300

    Add some fixes

---
+++ src/app/(admin)/(rgs)/games/page.tsx
@@ -0,0 +1,23 @@
+import PageBreadcrumb from "@/components/common/PageBreadCrumb";
+import { Metadata } from "next";
+import React from "react";
+import ComponentCard from "@/components/common/ComponentCard";
+import BasicTable from "@/app/(admin)/(rgs)/games/BasicTable";
+
+export const metadata: Metadata = {
+    title: "Next.js Blank Page | TailAdmin - Next.js Dashboard Template",
+    description: "This is Next.js Blank Page TailAdmin Dashboard Template",
+};
+
+export default function GamesPage() {
+    return (
+        <div>
+            <PageBreadcrumb pageTitle="All Games"/>
+            <div className="space-y-6">
+                <ComponentCard title="Games">
+                    <BasicTable/>
+                </ComponentCard>
+            </div>
+        </div>
+    );
+}
