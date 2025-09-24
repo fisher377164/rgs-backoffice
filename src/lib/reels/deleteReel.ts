dfb41c2a25cf7485c8752e0d8e9856b66dd969de
Andrii Vdovychenko <fisher377164@gmail.com>
2025-09-24 03:28:04 +0300

    Revert "feat: redesign reel sets management"
    This reverts commit 77026884627bf085e17238fc897960ed1a92a9b1.

--- src/lib/reels/deleteReel.ts
+++
@@ -1,7 +0,0 @@
-import { fetchData } from "@/lib/apiClient";
-
-export const deleteReel = async (id: number) => {
-  await fetchData<void>(`/v1/reels/${id}`, {
-    method: "DELETE",
-  });
-};
--- src/lib/reels/fetchReels.ts
+++
@@ -1,8 +0,0 @@
-import { Reel } from "@/lib/reels/reelType";
-import { fetchData } from "@/lib/apiClient";
-
-export const fetchReels = async (reelSetId: number) => {
-  const params = new URLSearchParams({ reelSetId: String(reelSetId) });
-
-  return fetchData<Reel[]>(`/v1/reels?${params.toString()}`);
-};
